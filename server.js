ff6792caf4029fcb0e98447dd2ff558730b81440
Cameron Pavao <cam9548@gmail.com>
2020-07-25 21:24:32 -0500

    maintain server list

--- server.js
+++ server.js
@@ -1,44 +1,72 @@
-const app = require('express')();
-const bodyParser = require('body-parser');
+const app = require("express")();
+const bodyParser = require("body-parser");
 
 // const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 const port = process.env.PORT || 5000;
-var serverList = [{
-  accessCode: null,
-  messages: [],
-  userList: []
-}];
+var serverList = [
+  {
+    accessCode: null,
+    messages: [],
+    userList: []
+  }
+];
 
-io.on('connection', (client) => {
-  console.log('a user connected');
+updateServerList = newServerItem => {
+  if (newServerItem) {
+    if (
+      !serverList.find(
+        serverListItem => serverListItem.accessCode === newServerItem.accessCode
+      )
+    ) {
+      serverList.push(newServerItem);
+      console.log("pushing new server list item: ", serverList);
+    }
+  }
+};
 
-  client.on('addUser', (user, accessCode) => {
-    console.log('add user', user, accessCode);
+io.on("connection", client => {
+  console.log("a user connected");
+  // console.log(serverList);
 
-    setInterval(() => {
-      const tempUserList = serverList.filter(entry => {
-        return entry.accessCode === accessCode
-      });
-      if (tempUserList.length > 0) {
-        tempUserList[0].userList.push(user);
-        console.log(tempUserList);
-        client.emit('updateUserList', tempUserList[0].userList);
-      }
-    }, 3000);
+  client.on("addUser", (user, accessCode) => {
+    console.log("add user", user, accessCode);
+    let newUserListItem;
+
+    if (!serverList[0].accessCode) {
+      newUserListItem = serverList[0];
+      newUserListItem.accessCode = accessCode;
+    } else {
+      newUserListItem = serverList.filter(
+        entry => entry.accessCode === accessCode
+      )[0] || {
+        accessCode: accessCode,
+        messages: [],
+        userList: []
+      };
+    }
+
+    if (newUserListItem) {
+      newUserListItem.userList.push(user);
+      updateServerList(newUserListItem);
+      client.emit("updateUserList", newUserListItem.userList);
+    } else if (tempUserList.includes(user)) {
+      console.log("User: " + user + " already exists");
+      client.emit("error", "user.exists");
+    }
   });
 
-  client.on('getServerList', (accessCode) => {
-  console.log('client is updating the server list with interval ');
+  client.on("getServerList", accessCode => {
+    console.log("client is updating the server list with interval ");
 
     setInterval(() => {
       const tempServer = serverList.filter(entry => {
-        return entry.accessCode === accessCode
+        return entry.accessCode === accessCode;
       });
-      console.log(tempServer);
+      // console.log(tempServer);
       if (tempServer.length > 0) {
-        client.emit('getListEmit', tempServer[0]);
+        client.emit("getListEmit", tempServer[0]);
       }
     }, 1000);
   });
@@ -47,15 +75,14 @@ io.on('connection', (client) => {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/client/public/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/client/public/index.html");
 });
 
-
-app.get('/getList', (req, res) => {
+app.get("/getList", (req, res) => {
   const accessCode = parseInt(req.query.code);
   const found = serverList.filter(entry => {
-    return entry.accessCode === accessCode
+    return entry.accessCode === accessCode;
   });
   if (found !== null && found.length > 0) {
     res.send(found);
@@ -64,7 +91,7 @@ app.get('/getList', (req, res) => {
   }
 });
 
-app.post('/updateList', (req, res) => {
+app.post("/updateList", (req, res) => {
   const reqAccessCode = parseInt(req.body.accessCode);
   const reqMessages = req.body.messages;
   const reqUserList = req.body.userList || [];
